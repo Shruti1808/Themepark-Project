dfb398524a2717236c6a68d03601fb0b3f9de480
Cassie Musolf and Shruti Priya <shruti@>
2017-01-12 13:11:20 -0800

    add final touches to themepark project.

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,14 @@
 var test= "it works";
 
 $(document).ready(function() {
-  var height = parseInt(prompt("How tall are you?( height in inches)"));
-  event.preventDefault();
 
-  if(height>44 inches) {
+  var height = parseInt(prompt("How tall are you?( height in inches)"));
+  // event.preventDefault();
+
+  if(height>44) {
     $(".adults").show();
     $(".kids").hide();
-  } else if (height===44 inches) {
+  } else if (height===44) {
     $(".adults, .kids").show();
   } else {
     $(".kids").show();
